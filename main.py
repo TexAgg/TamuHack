2c3b39d033e5868bf9dabecf973d418df81d3dcc
JasonZuang <zuangjason@gmail.com>
2016-10-01 13:05:37 -0500

    asd

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+#! /usr/bin/python
+import json
+from flask import Flask,request,render_template
+import requests
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def hello():
-    return "Hello World!"
-
+    return render_template("index.html")
+@app.route("/login")
+def login():
+	return render_template("login.html")
 if __name__ == "__main__":
     app.run()
