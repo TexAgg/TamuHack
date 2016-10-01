a1414aa68e90f4c925c43c2402da4d82ed696e31
JasonZuang <zuangjason@gmail.com>
2016-10-01 13:41:21 -0500

    asd

--- main.py
+++ main.py
@@ -10,8 +10,24 @@ app = Flask(__name__)
 @app.route("/index")
 def hello():
     return render_template("index.html")
-@app.route("/login")
+@app.route("/login", methods = ['GET','POST'])
 def login():
-	return render_template("login.html")
+	if request.method == "GET":
+		return render_template('login.html')
+	elif request.method == "POST":
+		name = request.form['FullName']
+		email = request.form['Email']
+		phone = request.form['Phone']
+		password = request.form['Password']
+		skills = request.form['Skills']
+		hacks = request.form['Hackathon']
+
+		print email
+		print hacks
+		return "done"
+	
+
+
+
 if __name__ == "__main__":
     app.run()
