54cba294c398fbad7b1a45860607dd973ad626dc
JasonZuang <zuangjason@gmail.com>
2016-10-02 01:37:08 -0500

    asd

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import requests
 from firebase import firebase
 import json
 from hackgetter import hackgetter
+import math
 
 from flask import Flask
 app = Flask(__name__)
 
 # pls dont steal database secret.
 db = firebase("https://tamuhack-62a7e.firebaseio.com/", "9aPTl5HogP4eHdKHdNhRWqiFH5WkOW1HeHrN30gD")
-
+result = [' ',' ',' ',' ',' ']
 @app.route("/")
 @app.route("/index")
 def hello():
@@ -40,7 +41,7 @@ def login(Events=None):
 			"password": password,
 			"skills": skills,
 			"hacks": hacks,
-			"members":1,
+			"members":"",
 			"full":False
 		}
 
@@ -62,6 +63,7 @@ def nearby():
 		c.download_html("https://mlh.io/seasons/na-2017/events")
 		events = c.get_hackathons()
 		return render_template("choose_hack.html", Events=events)
+
 
 def calculateall():
 	match(separate())
@@ -90,16 +92,21 @@ def separate():
 	for key,value in groups.iteritems():
 		print groups[key] 		
 
-	'''for key,value in groups.iteritems():
+	for key,value in groups.iteritems():
 		print key
 		print value
-	'''
+	
 	return groups
 
 def match(groups):
-	pass
+	print " "
+	match2(groups.values()[0])
 	
+def match2(listofdicks):
+	print'\n'
+	grouped = []
+	print listofdicks
 
 if __name__ == "__main__":
-	app.run()
+	calculateall()
 	
