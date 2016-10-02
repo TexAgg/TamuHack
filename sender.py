b98fe3f1a9af2320e6937cf6be1fa4af6ba6d59b
JasonZuang <zuangjason@gmail.com>
2016-10-01 20:34:28 -0500

    asd

---
+++ sender.py
@@ -0,0 +1,20 @@
+# /usr/bin/python
+
+import smtplib
+
+class sender:
+
+	def __init__(self,message,reciever):
+		self.__message__ = message
+		self.__reciever__ = reciever
+	
+	def sendEmail(self):
+		smtpObj = smtplib.SMTP('smtp.gmail.com',587)
+		smtpObj.ehlo()
+		smtpObj.starttls()
+		smtpObj.login('hackrteam@gmail.com', '244466666')
+		print smtpObj.sendmail('hackrteam@gmail.com', self.__reciever__,'Subject:Team Made! \n'+self.__message__)
+
+if __name__ =="__main__":
+	c = sender("Hey this is an automated voice message please hang up and dial again, jk its fucking ur older brother you dumbass and im about to fucking win this hackathon","kevinzuang@gmail.com")
+	c.sendEmail()
